cb97969dc902003421583cc5945be4729c0402ae
yasar2385 <yasar.mohideen@newgen.co>
2022-05-10 23:24:36 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,19 +6,4 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const app = document.getElementById('aaa');
 var upate = '';
-console.log('aaaa');
-var loop=0;
-const set = function () {
-  while (app.innerHTML.indexOf('&nbsp;&nbsp;')) {
-    console.log(loop);
-    upate = app.innerHTML.replaceAll('&nbsp;&nbsp;', '&nbsp;');
-    app.innerHTML = upate;
-    loop++;
-  }
-};
-setTimeout(function () {
-  console.log('eeee');
-  app.classList.add('bg-yellow');
-  set();
-}, 3000);
-console.log('dddd');
+console.log('aaadddda');
