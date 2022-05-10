68ee9271134cd9641a30ba5efa68f7921781dd1c
yasar2385 <yasar.mohideen@newgen.co>
2022-05-10 23:24:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const app = document.getElementById('aaa');
 var upate = '';
 console.log('aaadddda');
+var loop=0;
+const set = function () {
+  while (app.innerHTML.indexOf('&nbsp;&nbsp;')) {
+    console.log(loop);
+    upate = app.innerHTML.replaceAll('&nbsp;&nbsp;', '&nbsp;');
+    app.innerHTML = upate;
+    loop++;
+  }
+};
+setTimeout(function () {
+  console.log('eeee');
+  app.classList.add('bg-yellow');
+  set();
+}, 3000);
+console.log('dddd');
