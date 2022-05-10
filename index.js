9571a38328e7d54b0b40a742668123a73d075229
yasar2385 <yasar.mohideen@newgen.co>
2022-05-10 23:21:11 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const app = document.getElementById('aaa');
 var upate = '';
+console.log('aaaa');
 const set = function () {
   while (app.innerHTML.indexOf('&nbsp;&nbsp;')) {
     upate = app.innerHTML.replaceAll('&nbsp;&nbsp;', '&nbsp;');
